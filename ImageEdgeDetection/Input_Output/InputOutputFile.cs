e747f9ca0170d596210f2e858d66a62a889586cd
sylvaintauxe <sylvain.tauxe@students.hevs.ch>
2016-11-12 12:34:00 +0100

    Implémentation I O Layer

--- ImageEdgeDetection/Input_Output/InputOutputFile.cs
+++ ImageEdgeDetection/Input_Output/InputOutputFile.cs
@@ -1,11 +1,74 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Windows.Forms;
 
-namespace ImageEdgeDetection.Input_Output
+namespace ImageEdgeDetection.Input_Output 
 {
-    class InputOutputFile
+   public  class InputOutputFile : IImageManipulation
     {
+        Bitmap importedBitmap;
+        //Bitmap resultBitmap;
+        public Bitmap ImportBitmap()
+        {
+            //Display windows to import image
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Select an image file.";
+            ofd.Filter = "Png Images(*.png)|*.png|Jpeg Images(*.jpg)|*.jpg";
+            ofd.Filter += "|Bitmap Images(*.bmp)|*.bmp";
+
+            //When the "ok" button is pressed
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                StreamReader streamReader = new StreamReader(ofd.FileName);
+                importedBitmap = (Bitmap)Bitmap.FromStream(streamReader.BaseStream);
+                streamReader.Close();
+
+            }
+            return importedBitmap;
+           // throw new NotImplementedException();
+        }
+
+        public void SaveBitmap(Bitmap image)
+        {
+
+
+            if (image != null)
+            {
+                //Display windows to save image
+                SaveFileDialog sfd = new SaveFileDialog();
+                sfd.Title = "Specify a file name and file path";
+                sfd.Filter = "Png Images(*.png)|*.png|Jpeg Images(*.jpg)|*.jpg";
+                sfd.Filter += "|Bitmap Images(*.bmp)|*.bmp";
+
+                //When the "ok" button is pressed
+                if (sfd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    string fileExtension = Path.GetExtension(sfd.FileName).ToUpper();
+                    ImageFormat imgFormat = ImageFormat.Png;
+
+                    if (fileExtension == "BMP")
+                    {
+                        imgFormat = ImageFormat.Bmp;
+                    }
+                    else if (fileExtension == "JPG")
+                    {
+                        imgFormat = ImageFormat.Jpeg;
+                    }
+
+                    StreamWriter streamWriter = new StreamWriter(sfd.FileName, false);
+                    image.Save(streamWriter.BaseStream, imgFormat);
+                    streamWriter.Flush();
+                    streamWriter.Close();
+                }
+            }
+
+
+            // throw new NotImplementedException();
+        }
     }
 }
---
+++ UnitTestEdgeDetection/UnitTestInputOutput.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ImageEdgeDetection.Input_Output;
+using System.Drawing;
+
+namespace UnitTestEdgeDetection
+{
+    [TestClass]
+    public class UnitTestInputOutput
+    {
+        [TestMethod]
+        public void TestInput()
+        {
+            InputOutputFile iof = new InputOutputFile();
+            iof.ImportBitmap();
+        }
+
+        [TestMethod]
+        public void TestOutput()
+        {
+           InputOutputFile iof = new InputOutputFile();
+            
+            Bitmap img = iof.ImportBitmap();
+            iof.SaveBitmap(img);
+        }
+    }
+}
